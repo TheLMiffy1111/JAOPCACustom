38d4a22e1418eba6cf5740096ce13446c21c5c1a
TheLMiffy1111 <1111miffy@argentum.net>
2017-08-20 11:55:18 +0800

    1.0.3 changes
    Fixed fluid gaseous predicate

--- src/main/java/thelm/jaopca/custom/JAOPCACustom.java
+++ src/main/java/thelm/jaopca/custom/JAOPCACustom.java
@@ -18,7 +18,7 @@ import thelm.jaopca.custom.module.ModuleCustom;
 		)
 public class JAOPCACustom {
 	public static final String MOD_ID = "jaopcacustom";
-	public static final String VERSION = "1.10.2-1.0.0";
+	public static final String VERSION = "1.10.2-1.0.3";
 	@Instance(JAOPCACustom.MOD_ID)
 	public static JAOPCACustom core;
 	public static ModMetadata metadata;

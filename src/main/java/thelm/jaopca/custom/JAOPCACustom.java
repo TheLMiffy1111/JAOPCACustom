f5e4dd0d50197e0be648335b5abd3b30a684f522
TheLMiffy1111 <1111miffy@argentum.net>
2017-10-05 20:21:16 +0800

    2.1.1.10
    Now supports custom entry types added in JAOPCA

--- src/main/java/thelm/jaopca/custom/JAOPCACustom.java
+++ src/main/java/thelm/jaopca/custom/JAOPCACustom.java
@@ -14,11 +14,11 @@ import thelm.jaopca.custom.module.ModuleCustom;
 		modid = JAOPCACustom.MOD_ID,
 		name = "JAOPCASingularities",
 		version = JAOPCACustom.VERSION,
-		dependencies = "required-before:jaopca@[1.11.2-2.1.1.37,)"
+		dependencies = "required-before:jaopca@[1.11.2-2.1.3.46,)"
 		)
 public class JAOPCACustom {
 	public static final String MOD_ID = "jaopcacustom";
-	public static final String VERSION = "1.11.2-2.1.0.7";
+	public static final String VERSION = "1.11.2-2.1.1.10";
 	@Instance(JAOPCACustom.MOD_ID)
 	public static JAOPCACustom core;
 	public static ModMetadata metadata;
